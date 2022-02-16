99d408b556371bbd29d9c7ae181ccc104ebd8053
Russkih Anatoly <titanofdarkness@gmail.com>
2022-02-16 19:38:19 +1000

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 extern crate pdf;
 
-use std::env::args;
 use std::time::SystemTime;
-use std::fs;
-use std::collections::HashMap;
 
-use pdf::file::File;
-use pdf::object::*;
-use pdf::primitive::Primitive;
 use pdf::error::PdfError;
+use pdf::file::File;
 
 fn main() -> Result<(), PdfError> {
     let now = SystemTime::now();
 
     let file = File::<Vec<u8>>::open("./VULN-20220209.12.pdf").unwrap();
     if let Some(ref info) = file.trailer.info_dict {
-        let title: Option<String> = info.get("Title").and_then(|p| Some(p.try_into().unwrap()));
-        let author: Option<String> = info.get("Author").and_then(|p| Some(p.try_into().unwrap()));
+        let title: Option<String> = info.get("Title").map(|p| p.try_into().unwrap());
+        let author: Option<String> = info.get("Author").map(|p| p.try_into().unwrap());
 
         let descr = match (title, author) {
             (Some(title), None) => title,
@@ -25,15 +20,6 @@ fn main() -> Result<(), PdfError> {
             _ => "PDF".into(),
         };
         println!("{}", descr);
-    }
-
-    for page in file.pages() {
-        let page = page.unwrap();
-        let resources = page.resources().unwrap();
-        for (i, &font) in resources.fonts.values().enumerate() {
-            let font = file.get(font)?;
-            let name = &font.name;
-        }
     }
 
     if let Ok(elapsed) = now.elapsed() {
