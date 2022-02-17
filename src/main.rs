f0b28e1be8eddbc06c9ff474e86ff0425612bf0f
Russkih Anatoly <titanofdarkness@gmail.com>
2022-02-18 09:15:06 +1000

    Logs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 extern crate lopdf;
 extern crate pdf_extract;
 
+use simple_logger::SimpleLogger;
 use std::time::SystemTime;
 use vuln_pdf_parser::{get_pdf_files_in_directory, process_pdf_files};
 
 fn main() {
+    SimpleLogger::new().init().unwrap();
+    log::info!("App start");
     let now = SystemTime::now();
     let files = get_pdf_files_in_directory();
     process_pdf_files(&files);
 
     if let Ok(elapsed) = now.elapsed() {
-        println!(
+        log::info!(
             "Time: {}s",
             elapsed.as_secs() as f64 + elapsed.subsec_nanos() as f64 * 1e-9
         );
